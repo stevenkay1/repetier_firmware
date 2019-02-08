dcf21756fefa36056b01da0a2e2d44a7702c6105
Steven Kay <skay@gmv.com>
2019-02-08 22:05:36 +0000

    changes made to HAL.cpp to resolve stepperWait = 0 error#

--- HAL.cpp
+++ HAL.cpp
@@ -663,7 +663,10 @@ inline void setTimer(uint32_t delay)
 }
 
 volatile uint8_t insideTimer1 = 0;
-long stepperWait = 0;
+
+// Added to overcome stepperWait bug
+long __attribute__((used)) stepperWait = 0;
+
 /** \brief Timer interrupt routine to drive the stepper motors.
 */
 ISR(TIMER1_COMPA_vect)
@@ -1467,4 +1470,3 @@ RFHardwareSerial RFSerial(&rx_buffer, &tx_buffer, &UBRR0H, &UBRR0L, &UCSR0A, &UC
 #endif
 
 #endif
-
